5e32546afad6445f3acfaa6ea50b3da1facf2f00
Maximilien22 <55790623+Maximilien22@users.noreply.github.com>
2021-06-18 10:01:52 +0200

    Delete main.d

--- main.d
+++
@@ -1,4 +0,0 @@
-main: main.c Parsing/parsing.h Parsing/tools.h Parsing/vector.h \
- Parsing/tools.h Parsing/vector.h ShortestPath/path.h ShortestPath/list.h \
- ShortestPath/list.h Graphics/graphics.h Graphics/../Parsing/parsing.h \
- Graphics/../Parsing/tools.h Graphics/../ShortestPath/list.h
